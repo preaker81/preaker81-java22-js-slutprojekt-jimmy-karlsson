307a04e44e2223c7f2a03c2225c5c72cb872d06d
Jimmy Karlsson <47915308+preaker81@users.noreply.github.com>
2023-02-01 18:00:29 +0100

    Update galleryScript.js

--- js/galleryScript.js
+++ js/galleryScript.js
@@ -110,6 +110,8 @@ function scrollFunction() {
   }
 }
 
+document.querySelector('#backToTop').addEventListener('click', backToTopFunc);
+
 function backToTopFunc() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
